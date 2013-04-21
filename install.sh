2c47e1e68a15b49c2996be15dc52d72f5e039c3d
Jesper Brix Rosenkilde <jbr@humppa.dk>
2013-04-21 18:34:16 +0200

    Added install script

---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+VIM_PATH=~/.vim
+REPO="https://github.com/jbro/vim.git"
+
+while getopts dcp: flag; do
+  case $flag in
+    c)
+      REPO="git@github.com:jbro/vim.git"
+      ;;
+    p)
+      VIM_PATH=$OPTARG
+      ;;
+    d)
+      NO_SYMLINK=true
+      ;;
+    ?)
+      exit
+      ;;
+  esac
+done
+
+git clone --recursive $REPO $VIM_PATH
+
+cd $VIM_PATH
+./update.sh -r
+export LSCOLORS=GxFxCxDxBxegedabagaced
+
+if [ ! $NO_SYMLINK ]; then
+  ln -s ~/.vimrc $VIM_PATH/vimrc
+fi
+
